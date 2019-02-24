2aebeb80bc3982331ca4a468f72eb09eb5236466
TheEvilRoot <robot-creatix@ya.ru>
2019-02-24 17:37:38 +0300

    Initial Commit

---
+++ CUtilities/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CUtilities
+//
+//  Created by Доктор Кларик on 24.02.19.
+//  Copyright © 2019 Доктор Кларик. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
